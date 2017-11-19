99c3e451f0e2c8476548fa4df4161e77353bb3f6
Mars Howard <marsbhoward@gmail.com>
2017-11-19 14:42:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,10 +1,4 @@
 require_relative 'config/environment'
-gem 'sinatra'
-gem 'activerecord', '4.2.5'
-gem 'sinatra-activerecord'
-gem 'rake'
-gem 'thin'
-gem 'require_all'
 
 class App < Sinatra::Base
 end
